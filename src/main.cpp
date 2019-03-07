15c0de5950aac1d3982a1820858455d64f5fd815
Camille Leroux <camille.leroux@ims-bordeaux.fr>
2019-03-07 09:21:28 +0100

    cleanup in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,28 @@ int main(int argc, char** argv)
 
 	const int K_BCH = 14232;
 	const std::string mat2aff_file_name = "../matlab/matlab_to_aff3ct.txt";
-	std::ofstream aff2mat_file;
+	const std::string aff2mat_file_name = "../matlab/aff3ct_to_matlab.txt";
 
 	// buffers to store the data
-	std::vector<int  > scrambler_in ( K_BCH );
-	const std::vector<int  > lfsr_init{1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0};
-	std::vector<int  > lfsr(15);
-	int feedback;
+	      std::vector<int  > scrambler_in ( K_BCH );
+	const std::vector<int  > lfsr_init    {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0};
 
 	// create the AFF3CT objects
-	module::Source_user<int> source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
-	BB_scrambler my_scrambler;
-	Sink sink_to_matlab("../matlab/aff3ct_to_matlab.txt");
-	tools::Frame_trace<> tracer(15, 1, std::cout);
+	module::Source_user<int> 	source_from_matlab	(K_BCH, mat2aff_file_name);//, 1, 1);
+	BB_scrambler 				my_scrambler;
+	Sink 						sink_to_matlab		(aff2mat_file_name);
+	tools::	Frame_trace<> 		tracer				(15, 1, std::cout);
 
 	// retrieve data from Matlab
 	source_from_matlab.generate( scrambler_in );
 	
 	// Base Band scrambling
-	my_scrambler.scramble(scrambler_in);
+	my_scrambler.scramble( scrambler_in );
 
 	// Pushes data to Matlab
-	sink_to_matlab.push_vector(scrambler_in);
+	sink_to_matlab.push_vector( scrambler_in );
 
-	//tracer.display_bit_vector(scrambler_in);
+	//tracer.display_bit_vector( scrambler_in );
 
 	return 0;
 }
