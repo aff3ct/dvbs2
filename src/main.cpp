9aba870deb5706733abf51a98cd225032d506c97
Mathieu Léonardon <mathieu.leonardon@gmail.com>
2019-03-07 12:06:06 +0100

    Refactor.

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,23 @@
  #include <aff3ct.hpp>
  #include "Sink.hpp"
  #include "BB_scrambler.hpp"
- //#include <vector>
- //#include <algorithm>
 
 int main(int argc, char** argv)
 {
 	using namespace aff3ct;
 
 	const int K_BCH = 14232;
-	const std::string mat2aff_file_name = "../matlab/matlab_to_aff3ct.txt";
-	const std::string aff2mat_file_name = "../matlab/aff3ct_to_matlab.txt";
+	const std::string mat2aff_file_name = "../build/matlab_to_aff3ct.txt";
+	const std::string aff2mat_file_name = "../build/aff3ct_to_matlab.txt";
 
 	// buffers to store the data
-	      std::vector<int  > scrambler_in ( K_BCH );
-	const std::vector<int  > lfsr_init    {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+	std::vector<int  > scrambler_in(K_BCH);
 
 	// create the AFF3CT objects
-	module::Source_user<int> 	source_from_matlab	(K_BCH, mat2aff_file_name);//, 1, 1);
-	BB_scrambler 				my_scrambler;
-	Sink 						sink_to_matlab		(aff2mat_file_name);
-	tools::	Frame_trace<> 		tracer				(15, 1, std::cout);
+	module::Source_user<int> source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
+	BB_scrambler             my_scrambler;
+	Sink                     sink_to_matlab    (aff2mat_file_name);
+	tools::Frame_trace<>     tracer            (15, 1, std::cout);
 
 	// retrieve data from Matlab
 	source_from_matlab.generate( scrambler_in );
