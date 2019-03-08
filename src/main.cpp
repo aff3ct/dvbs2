3032cb0d3804b9f1fe11b8cbd0785ba2fdf2033d
Camille Leroux <camille.leroux@ims-bordeaux.fr>
2019-03-08 17:52:01 +0100

    BCH encoder works.

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,31 @@ int main(int argc, char** argv)
 	using namespace aff3ct;
 
 	const int K_BCH = 14232;
+	const int N_BCH = 14400;
+
+	const std::vector<int  > BCH_gen_poly{1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1};
 	const std::string mat2aff_file_name = "../build/matlab_to_aff3ct.txt";
 	const std::string aff2mat_file_name = "../build/aff3ct_to_matlab.txt";
 
 	// buffers to store the data
 	std::vector<int  > scrambler_in(K_BCH);
 
-	// create the AFF3CT objects
-	module::Source_user<int> source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
+	std::vector<int  > bch_enc_in(K_BCH);
+	std::vector<int  > bch_encoded(N_BCH);
+
+	std::vector<int  > parity(N_BCH-K_BCH);
+	std::vector<int  > msg(K_BCH);
+
+	// Tracer
+	tools::Frame_trace<>     tracer            (15, 10, std::cout);
+	
+	// Create the AFF3CT objects
+	module::Source_user<int > source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
 	BB_scrambler             my_scrambler;
+	tools::BCH_polynomial_generator<int  > poly_gen(16383, 12);
+	poly_gen.set_g(BCH_gen_poly);
+	module::Encoder_BCH<int > BCH_encoder(K_BCH, N_BCH, poly_gen, 1);
 	Sink                     sink_to_matlab    (aff2mat_file_name);
-	tools::Frame_trace<>     tracer            (15, 1, std::cout);
 
 	// retrieve data from Matlab
 	source_from_matlab.generate( scrambler_in );
@@ -27,10 +41,26 @@ int main(int argc, char** argv)
 	// Base Band scrambling
 	my_scrambler.scramble( scrambler_in );
 
+	// reverse message for Matlab compliance
+	std:reverse(scrambler_in.begin(), scrambler_in.end()); 
+
+	// BCH Encoding
+	BCH_encoder.encode(scrambler_in, bch_encoded);
+
+	// revert parity and msg for Matlab compliance
+	parity.assign(bch_encoded.begin(), bch_encoded.begin()+(N_BCH-K_BCH)); // retrieve parity
+	std::reverse(parity.begin(), parity.end()); // revert parity bits
+	msg.assign(bch_encoded.begin()+(N_BCH-K_BCH), bch_encoded.end()); // retrieve message
+	std::reverse(msg.begin(), msg.end()); // revert msg bits
+
+	// swap parity and msg for Matlab compliance
+	bch_encoded.insert(bch_encoded.begin(), msg.begin(), msg.end()); 
+	bch_encoded.insert(bch_encoded.begin()+K_BCH, parity.begin(), parity.end());
+
+	bch_encoded.erase(bch_encoded.begin()+N_BCH, bch_encoded.end());
+	
 	// Pushes data to Matlab
-	sink_to_matlab.push_vector( scrambler_in, false);
-
-	//tracer.display_bit_vector( scrambler_in );
-
+	sink_to_matlab.push_vector( bch_encoded , false);
+	
 	return 0;
 }
