c125cbc171c0b90997ea02a44620eda4805a8038
Romain Tajan <romain.tajan@ims-bordeaux.fr>
2019-03-11 16:31:25 +0100

    Modification of Sink.hpp for bidirectionnal / float / complex communications.

--- src/Sink.hpp
+++ src/Sink.hpp
@@ -10,6 +10,13 @@
 
 #include <vector>
 #include <iostream>
+#include <string>
+
+#include <aff3ct.hpp>
+
+
+using namespace aff3ct;
+using namespace aff3ct::module;
 
 /*!
  * \class Sink
@@ -22,21 +29,127 @@ class Sink
 {
 
 private:
-
-	const std::string filename;
-	std::ofstream output_file;
+	const std::string input_filename;
+	const std::string output_filename;
 	
+	std::ifstream     input_file;
+	std::ofstream     output_file;
+
+	std::vector<std::vector<int> > source;
+	int src_counter;
+
+	void pull_vector(int* vec, int N);
 	void push_vector(int* vec, int N);
 
 public:
-	Sink(std::string filename);
+	std::string destination_chain_name;
+
+	Sink(std::string input_filename, std::string output_filename);
 	~Sink();
 
-	void push_vector(std::vector<int  > vec, bool iscomplex);
-	void push_vector(std::vector<float  > vec, bool iscomplex);
-	void push_vector(std::vector<double  > vec, bool iscomplex);
+	void push_vector(std::vector<int   > vec, bool iscomplex);
+	void push_vector(std::vector<float > vec, bool iscomplex);
+	void push_vector(std::vector<double> vec, bool iscomplex);
+
+	void pull_vector(std::vector<int   > &vec);
+	void pull_vector(std::vector<float > &vec);
+	void pull_vector(std::vector<double> &vec);
 };
 //#include "Sink.cpp"
+
+Sink
+::Sink(const std::string input_filename, const std::string output_filename)
+: input_filename(input_filename), output_filename(output_filename), input_file(std::ifstream()), output_file(std::ofstream()), source(), src_counter(0), destination_chain_name()
+{
+	if (input_filename.empty())
+		throw tools::invalid_argument(__FILE__, __LINE__, __func__, "'input_filename' should not be empty.");
+
+	if (output_filename.empty())
+		throw tools::invalid_argument(__FILE__, __LINE__, __func__, "'input_filename' should not be empty.");
+
+	input_file.open (input_filename);
+	output_file.open(output_filename);
+
+	if (input_file.is_open())
+	{
+		int n_src = 0, src_size = 0;
+		std::getline(input_file, destination_chain_name);
+
+		input_file >> n_src;
+		input_file >> src_size;
+
+		if (n_src <= 0 || src_size <= 0)
+		{
+			std::stringstream message;
+			message << "'n_src', and 'src_size' have to be greater than 0 ('n_src' = " << n_src
+			        << ", 'src_size' = " << src_size << ").";
+			throw tools::runtime_error(__FILE__, __LINE__, __func__, message.str());
+		}
+
+		this->source.resize(n_src);
+		for (auto i = 0; i < n_src; i++)
+			this->source[i].resize(src_size);
+
+		for (auto i = 0; i < n_src; i++)
+			for (auto j = 0; j < src_size; j++)
+			{
+				int bit;
+				input_file >> bit;
+
+				this->source[i][j] = bit;
+			}
+
+		input_file.close();
+	}
+	else
+		throw tools::invalid_argument(__FILE__, __LINE__, __func__, "Can't open '" + input_filename + "' file.");
+
+	src_counter %= (int)source.size();
+}
+
+
+Sink
+::~Sink()
+{
+	input_file.close();
+	output_file.close();
+}
+
+
+void Sink
+::pull_vector(int *vec, const int N)
+{
+	if (N != this->source[this->src_counter].size())
+		throw tools::invalid_argument(__FILE__, __LINE__, __func__, "Wrong source size.");
+
+	std::copy(this->source[this->src_counter].begin(),
+	          this->source[this->src_counter].end  (),
+	          vec);
+
+	this->src_counter = (this->src_counter +1) % (int)this->source.size();
+}
+
+void Sink
+:: pull_vector(std::vector<int   > &vec)
+{
+	pull_vector(vec.data(), vec.size());
+}
+
+void Sink
+:: pull_vector(std::vector<float > &vec)
+{
+	int* int_data_ptr = reinterpret_cast<int *>(vec.data());
+	int  int_data_sz  = vec.size()*sizeof(float)/sizeof(int);
+	pull_vector(int_data_ptr, int_data_sz);
+}
+
+void Sink
+:: pull_vector(std::vector<double> &vec)
+{
+	int* int_data_ptr = reinterpret_cast<int *>(vec.data());
+	int  int_data_sz  = vec.size()*sizeof(double)/sizeof(int);
+	pull_vector(int_data_ptr, int_data_sz);
+}
 
 void Sink
 ::push_vector(int* vec, int N)
@@ -47,19 +160,6 @@ void Sink
 	output_file << std::endl;
 };
 
-Sink
-::Sink(std::string filename)
-: filename(filename), output_file(std::ofstream())
-{
-	output_file.open (filename);
-}
-
-
-Sink
-::~Sink()
-{
-	output_file.close();
-}
 
 void Sink::
 push_vector(std::vector<int  > vec, bool is_complex)
--- src/main.cpp
+++ src/main.cpp
@@ -8,59 +8,66 @@ int main(int argc, char** argv)
 {
 	using namespace aff3ct;
 
-	const int K_BCH = 14232;
-	const int N_BCH = 14400;
-
-	const std::vector<int  > BCH_gen_poly{1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1};
 	const std::string mat2aff_file_name = "../build/matlab_to_aff3ct.txt";
 	const std::string aff2mat_file_name = "../build/aff3ct_to_matlab.txt";
+	Sink sink_to_matlab    (mat2aff_file_name,aff2mat_file_name);
 
-	// buffers to store the data
-	std::vector<int  > scrambler_in(K_BCH);
+	if (sink_to_matlab.destination_chain_name == "coding")
+	{
+		const int K_BCH = 14232;
+		const int N_BCH = 14400;
 
-	std::vector<int  > bch_enc_in(K_BCH);
-	std::vector<int  > bch_encoded(N_BCH);
+		const std::vector<int  > BCH_gen_poly{1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1};
+		// buffers to store the data
+		std::vector<int  > scrambler_in(K_BCH);
 
-	std::vector<int  > parity(N_BCH-K_BCH);
-	std::vector<int  > msg(K_BCH);
+		std::vector<int  > bch_enc_in(K_BCH);
+		std::vector<int  > bch_encoded(N_BCH);
 
-	// Tracer
-	tools::Frame_trace<>     tracer            (15, 10, std::cout);
-	
-	// Create the AFF3CT objects
-	module::Source_user<int > source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
-	BB_scrambler             my_scrambler;
-	tools::BCH_polynomial_generator<int  > poly_gen(16383, 12);
-	poly_gen.set_g(BCH_gen_poly);
-	module::Encoder_BCH<int > BCH_encoder(K_BCH, N_BCH, poly_gen, 1);
-	Sink                     sink_to_matlab    (aff2mat_file_name);
+		std::vector<int  > parity(N_BCH-K_BCH);
+		std::vector<int  > msg(K_BCH);
 
-	// retrieve data from Matlab
-	source_from_matlab.generate( scrambler_in );
-	
-	// Base Band scrambling
-	my_scrambler.scramble( scrambler_in );
+		// Tracer
+		tools::Frame_trace<>     tracer            (15, 10, std::cout);
+		
+		// Create the AFF3CT objects
+		//module::Source_user<int > source_from_matlab(K_BCH, mat2aff_file_name);//, 1, 1);
+		BB_scrambler             my_scrambler;
+		tools::BCH_polynomial_generator<int  > poly_gen(16383, 12);
+		poly_gen.set_g(BCH_gen_poly);
+		module::Encoder_BCH<int > BCH_encoder(K_BCH, N_BCH, poly_gen, 1);
+		
+		// retrieve data from Matlab
+		sink_to_matlab.pull_vector( scrambler_in );
+		
+		// Base Band scrambling
+		my_scrambler.scramble( scrambler_in );
 
-	// reverse message for Matlab compliance
-	std:reverse(scrambler_in.begin(), scrambler_in.end()); 
+		// reverse message for Matlab compliance
+		std:reverse(scrambler_in.begin(), scrambler_in.end()); 
 
-	// BCH Encoding
-	BCH_encoder.encode(scrambler_in, bch_encoded);
+		// BCH Encoding
+		BCH_encoder.encode(scrambler_in, bch_encoded);
 
-	// revert parity and msg for Matlab compliance
-	parity.assign(bch_encoded.begin(), bch_encoded.begin()+(N_BCH-K_BCH)); // retrieve parity
-	std::reverse(parity.begin(), parity.end()); // revert parity bits
-	msg.assign(bch_encoded.begin()+(N_BCH-K_BCH), bch_encoded.end()); // retrieve message
-	std::reverse(msg.begin(), msg.end()); // revert msg bits
+		// revert parity and msg for Matlab compliance
+		parity.assign(bch_encoded.begin(), bch_encoded.begin()+(N_BCH-K_BCH)); // retrieve parity
+		std::reverse(parity.begin(), parity.end()); // revert parity bits
+		msg.assign(bch_encoded.begin()+(N_BCH-K_BCH), bch_encoded.end()); // retrieve message
+		std::reverse(msg.begin(), msg.end()); // revert msg bits
 
-	// swap parity and msg for Matlab compliance
-	bch_encoded.insert(bch_encoded.begin(), msg.begin(), msg.end()); 
-	bch_encoded.insert(bch_encoded.begin()+K_BCH, parity.begin(), parity.end());
+		// swap parity and msg for Matlab compliance
+		bch_encoded.insert(bch_encoded.begin(), msg.begin(), msg.end()); 
+		bch_encoded.insert(bch_encoded.begin()+K_BCH, parity.begin(), parity.end());
 
-	bch_encoded.erase(bch_encoded.begin()+N_BCH, bch_encoded.end());
-	
-	// Pushes data to Matlab
-	sink_to_matlab.push_vector( bch_encoded , false);
+		bch_encoded.erase(bch_encoded.begin()+N_BCH, bch_encoded.end());
+		
+		// Pushes data to Matlab
+		sink_to_matlab.push_vector( bch_encoded , false);
+	}
+	else
+	{
+		throw tools::invalid_argument(__FILE__, __LINE__, __func__, "Invalid destination name.");
+	}
 	
 	return 0;
 }
