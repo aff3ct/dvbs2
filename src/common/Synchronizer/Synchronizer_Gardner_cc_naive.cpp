f638a737be28e677f9d037240039dfdb4da912f7
Romain Tajan <romain.tajan@ims-bordeaux.fr>
2019-07-11 13:15:58 +0200

    Add timing recovery

---
+++ src/common/Synchronizer/Synchronizer_Gardner_cc_naive.cpp
@@ -0,0 +1,174 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <complex>
+
+#include "Synchronizer_Gardner_cc_naive.hpp"
+
+// _USE_MATH_DEFINES does not seem to work on MSVC...
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
+
+using namespace aff3ct::module;
+
+template <typename R>
+Synchronizer_Gardner_cc_naive<R>
+::Synchronizer_Gardner_cc_naive(const int N, int OSF)
+: Synchronizer<R>(N,N/OSF),
+OSF(OSF),
+POW_OSF(1<<OSF),
+INV_OSF((R)1.0/ (R)OSF),
+last_symbol(0,0),
+symbols_buffer(2*N, std::complex<R>(0,0)),
+N_symbol(0),
+mu(0),
+farrow_flt(N,(R)0),
+strobe_history(0),
+is_strobe(0),
+TED_error((R)0),
+TED_buffer(OSF, std::complex<R>(0,0)),
+TED_head_pos(OSF - 1),
+TED_mid_pos((OSF - 1 - OSF / 2) % OSF),
+TED_old_head_pos(0),
+set_bits_nbr(std::vector<int>{0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4}),
+lf_proportional_gain(-1.6666e-05),
+lf_integrator_gain   (-2.7777e-10),
+lf_prev_in ((R)0),
+lf_filter_state ((R)0),
+lf_output((R)0),
+NCO_counter((R)0)
+{
+}
+
+template <typename R>
+Synchronizer_Gardner_cc_naive<R>
+::~Synchronizer_Gardner_cc_naive()
+{}
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::_synchronize(const R *X_N1, R *Y_N2, const int frame_id)
+{
+	auto cX_N1 = reinterpret_cast<const std::complex<R>* >(X_N1);
+	
+	int n_in = 0;
+	int n_out = 0;
+	if((*this)[syn::tsk::synchronize].is_debug())
+		std::cout << "# {DEBUG} Mu = [" << this->get_mu() << "]"<< std::endl;
+		
+	for (auto i = 0; i < this->N_in/2 ; i++)
+	{
+		n_in += 2;
+		if (this->is_strobe == 1)
+		{
+			this->step(&cX_N1[i]);
+			if (2*n_out < this->N_out)
+			{
+				Y_N2[2 * n_out    ] = std::real(this->last_symbol);
+				Y_N2[2 * n_out + 1] = std::imag(this->last_symbol);
+			}
+			n_out += 1;
+		}
+		else
+		{
+			this->step(&cX_N1[i]);
+		}
+	}
+	//std::cerr << "N_in "<< n_in << " / " << this->N_in << "\n" << "N_out "<< 2*n_out << " / " << this->N_out << std::endl;
+}
+
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::step(const std::complex<R> *X_N1)
+{
+	std::complex<R> farrow_output(0,0);
+	farrow_flt.step( X_N1, &farrow_output );
+	
+	if (this->is_strobe)
+		this->last_symbol = farrow_output;
+
+	this->TED_update(farrow_output);
+
+	this->loop_filter();
+	
+	this->interpolation_control();
+}
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::reset()
+{
+}
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::loop_filter()
+{
+	//this->lf_filter_state += this->lf_prev_in;
+	//this->lf_output        = this->TED_error * this->lf_proportional_gain + this->lf_filter_state;
+	//this->lf_prev_in       = this->TED_error * this->lf_integrator_gain;
+	
+	R vp = this->TED_error * this->lf_proportional_gain;
+	R vi = this->lf_prev_in + this->TED_error * this->lf_integrator_gain;
+	this->lf_prev_in = vi;
+	this->lf_output = vp + vi;
+}
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::interpolation_control()
+{
+	// Interpolation Control
+	R W = this->lf_output + this->INV_OSF;
+	this->is_strobe = (this->NCO_counter < W) ? 1:0; // Check if a strobe
+	if (this->is_strobe == 1) // Update mu if a strobe
+	{
+		this->mu = this->NCO_counter / W;
+		this->farrow_flt.set_mu(this->mu);
+	}
+		
+	this->NCO_counter -= W + std::floor(this->NCO_counter - W); // Update counter*/
+}
+
+template <typename R>
+void Synchronizer_Gardner_cc_naive<R>
+::TED_update(std::complex<R> sample)
+{
+	this->strobe_history = (this->strobe_history << 1) % this->POW_OSF + this->is_strobe; 
+
+	if (this->strobe_history == 1)
+		this->TED_error = std::real(this->TED_buffer[this->TED_mid_pos]) * (std::real(this->TED_buffer[this->TED_head_pos]) - std::real(sample)) + 
+		                  std::imag(this->TED_buffer[this->TED_mid_pos]) * (std::imag(this->TED_buffer[this->TED_head_pos]) - std::imag(sample));
+	else
+		this->TED_error = 0.0f;
+
+	// Stuffing / skipping
+	switch (this->set_bits_nbr[this->strobe_history])
+	{
+		case 0:
+		break;
+
+		case 1:
+			this->TED_buffer[this->TED_head_pos] = sample;
+			this->TED_old_head_pos = this->TED_head_pos;
+			this->TED_head_pos = (this->TED_head_pos + 1)%this->OSF;
+			this->TED_mid_pos  = (this->TED_mid_pos  + 1)%this->OSF;
+		break;
+
+		default:
+			this->TED_buffer[ this->TED_head_pos               ] = R(0.0f);
+			this->TED_buffer[(this->TED_head_pos + 1)%this->OSF] = sample;
+
+			this->TED_old_head_pos = (this->TED_head_pos + 1) % this->OSF;
+			this->TED_head_pos     = (this->TED_head_pos + 2) % this->OSF;
+			this->TED_mid_pos      = (this->TED_mid_pos  + 2) % this->OSF;
+		break;
+	}
+}
+
+// ==================================================================================== explicit template instantiation
+template class aff3ct::module::Synchronizer_Gardner_cc_naive<float>;
+template class aff3ct::module::Synchronizer_Gardner_cc_naive<double>;
+// ==================================================================================== explicit template instantiation
---
+++ src/common/Synchronizer/Synchronizer_Gardner_cc_naive.hpp
@@ -0,0 +1,69 @@
+#ifndef SYNCHRONIZER_GARDNER_CC_NAIVE_HPP
+#define SYNCHRONIZER_GARDNER_CC_NAIVE_HPP
+
+#include <vector>
+#include <complex>
+
+#include "Synchronizer.hpp"
+#include "../Filter/Filter_FIR/Farrow/Filter_Farrow_ccr_naive.hpp"
+
+namespace aff3ct
+{
+namespace module
+{
+template <typename R = float>
+class Synchronizer_Gardner_cc_naive : public Synchronizer<R>
+{
+private:
+	const int OSF;
+	const int POW_OSF;
+	const R   INV_OSF;
+
+	std::complex<R> last_symbol;
+	std::vector<std::complex<R> > symbols_buffer;
+	int N_symbol;
+
+	// Interpolation parametes
+	R mu;
+	Filter_Farrow_ccr_naive <R>   farrow_flt;
+
+	// TED parameters
+	int strobe_history;
+	int is_strobe;
+	R TED_error;
+	std::vector<std::complex<R> > TED_buffer;
+	int TED_head_pos;
+	int TED_mid_pos;
+	int TED_old_head_pos;
+	const std::vector<int>  set_bits_nbr;
+
+	// Loop filter parameters
+	R lf_proportional_gain; // AIB -1.6666e-05; //  | -0.002951146572088;    // Matlab default
+	R lf_integrator_gain; // AIB-2.7777e-10; // | -5.902293144176643e-06;// Matlab default
+	R lf_prev_in;
+	R lf_filter_state;
+	R lf_output;
+
+	R NCO_counter;
+
+	void TED_update(std::complex<R> strobe);
+	void loop_filter();
+	void interpolation_control();
+	
+
+public:
+	Synchronizer_Gardner_cc_naive (const int N, int OSF);
+	virtual ~Synchronizer_Gardner_cc_naive();
+	void reset();
+
+	void step(const std::complex<R> *X_N1);
+	R get_mu() {return this->mu;};
+protected:
+	void _synchronize(const R *X_N1,  R *Y_N2, const int frame_id);
+
+};
+
+}
+}
+
+#endif //SYNCHRONIZER_GARDNER_CC_NAIVE_HPP
