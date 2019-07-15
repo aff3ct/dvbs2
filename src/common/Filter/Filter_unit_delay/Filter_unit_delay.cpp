e76b1e4418a9f48504f45baa79d0f345e58d8b3e
Romain Tajan <romain.tajan@ims-bordeaux.fr>
2019-07-15 15:11:46 +0200

    Fix bug in frame synchronization

---
+++ src/common/Filter/Filter_unit_delay/Filter_unit_delay.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <complex>
+
+#include "Filter_unit_delay.hpp"
+using namespace aff3ct::module;
+
+template <typename R>
+Filter_unit_delay<R>
+::Filter_unit_delay(const int N)
+: Filter<R>(N,N), mem(N, R(0))
+{
+}
+
+template <typename R>
+Filter_unit_delay<R>
+::~Filter_unit_delay()
+{}
+
+template <typename R>
+void Filter_unit_delay<R>
+::_filter(const R *X_N1, R *Y_N2, const int frame_id)
+{
+	for(auto i = 0; i<this->N; i++)
+	{
+		Y_N2[i]      = this->mem[i];
+		this->mem[i] = X_N1[i];
+	}
+}
+
+
+template <typename R>
+void Filter_unit_delay<R>
+::reset()
+{
+	for (size_t i = 0; i<this->N; i++)
+		this->mem[i] = (R)0;
+}
+
+// ==================================================================================== explicit template instantiation
+template class aff3ct::module::Filter_unit_delay<int>;
+template class aff3ct::module::Filter_unit_delay<float>;
+template class aff3ct::module::Filter_unit_delay<double>;
+// ==================================================================================== explicit template instantiation
