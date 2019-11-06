3f13ff7a94d95a1b1f4e30a86dfd5c9b0884c565
Mathieu Léonardon <mathieu.leonardon@gmail.com>
2019-11-06 13:48:43 +0000

    Refacto.

--- src/common/Params_DVBS2O/Params_DVBS2O.cpp
+++ src/common/Params_DVBS2O/Params_DVBS2O.cpp
@@ -11,23 +11,82 @@ Params_DVBS2O(int argc, char** argv)
 	cli::Argument_map_value arg_vals;
 	get_arguments(argc, argv, arg_vals);
 
+	modcod         = arg_vals.exist({"mod-cod"}           ) ? arg_vals.at      ({"mod-cod"}           ) : "QPSK-S_8/9";
+
+	modcod_init(modcod); // initialize all the parameters that are dependant on modcod
+
 	ebn0_min       = arg_vals.exist({"sim-noise-min","m"} ) ? arg_vals.to_float({"sim-noise-min","m"} ) : 3.2f        ;
 	ebn0_max       = arg_vals.exist({"sim-noise-max","M"} ) ? arg_vals.to_float({"sim-noise-max","M"} ) : 6.f         ;
 	ebn0_step      = arg_vals.exist({"sim-noise-step","s"}) ? arg_vals.to_float({"sim-noise-step","s"}) : .1f         ;
+	max_freq_shift = arg_vals.exist({"chn-max-freq-shift"}) ? arg_vals.to_float({"chn-max-freq-shift"}) : 0.f         ;
+	max_delay      = arg_vals.exist({"chn-max-delay"}     ) ? arg_vals.to_float({"chn-max-delay"}     ) : 0.f         ;
+	ldpc_nite      = arg_vals.exist({"dec-ite"}           ) ? arg_vals.to_int  ({"dec-ite"}           ) : 50          ;
+	max_fe         = arg_vals.exist({"max-fe","e"}        ) ? arg_vals.to_int  ({"max-fe","e"}        ) : 100         ;
+	sink_path      = arg_vals.exist({"snk-path"}          ) ? arg_vals.at      ({"snk-path"}          ) : ""          ;
+	ldpc_implem    = arg_vals.exist({"dec-implem"}        ) ? arg_vals.at      ({"dec-implem"}        ) : "SPA"       ;
+	ldpc_simd      = arg_vals.exist({"dec-simd"}          ) ? arg_vals.at      ({"dec-simd"}          ) : ""          ;
+	section        = arg_vals.exist({"section"}           ) ? arg_vals.at      ({"section"}           ) : ""          ;
+	src_type       = arg_vals.exist({"src-type"}          ) ? arg_vals.at      ({"src-type"}          ) : "RAND"      ;
+	src_path       = arg_vals.exist({"src-path"}          ) ? arg_vals.at      ({"src-path"}          ) : src_path    ;
 	debug          = arg_vals.exist({"sim-debug","d"}     ) ? true                                      : false       ;
 	stats          = arg_vals.exist({"sim-stats"}         ) ? true                                      : false       ;
 	no_pll         = arg_vals.exist({"no-pll"}            ) ? true                                      : false       ;
-	ldpc_nite      = arg_vals.exist({"dec-ite"}           ) ? arg_vals.to_int  ({"dec-ite"}           ) : 50          ;
-	ldpc_implem    = arg_vals.exist({"dec-implem"}        ) ? arg_vals.at      ({"dec-implem"}        ) : "SPA"       ;
-	ldpc_simd      = arg_vals.exist({"dec-simd"}          ) ? arg_vals.at      ({"dec-simd"}          ) : ""          ;
-	max_fe         = arg_vals.exist({"max-fe","e"}        ) ? arg_vals.to_int  ({"max-fe","e"}        ) : 100         ;
-	max_freq_shift = arg_vals.exist({"chn-max-freq-shift"}) ? arg_vals.to_float({"chn-max-freq-shift"}) : 0.f         ;
-	max_delay      = arg_vals.exist({"chn-max-delay"}     ) ? arg_vals.to_float({"chn-max-delay"}     ) : 0.f         ;
-	modcod         = arg_vals.exist({"mod-cod"}           ) ? arg_vals.at      ({"mod-cod"}           ) : "QPSK-S_8/9";
-	sink_path      = arg_vals.exist({"snk-path"}          ) ? arg_vals.at      ({"snk-path"}          ) : ""          ;
-	section        = arg_vals.exist({"section"}           ) ? arg_vals.at      ({"section"}           ) : ""          ;
-	src_type       = arg_vals.exist({"src-type"}          ) ? arg_vals.at      ({"src-type"}          ) : "RAND"      ;
 
+	p_rad.N = (this->pl_frame_size) * 4; // 2 * N_fil
+	p_rad.store(arg_vals);
+}
+
+void Params_DVBS2O::
+get_arguments(int argc, char** argv, cli::Argument_map_value& arg_vals)
+{
+		// build argument handler
+	cli::Argument_handler ah(argc, (const char**) argv);
+	cli::Argument_map_info args;
+	std::vector<std::string> cmd_warn, cmd_error;
+
+	p_rad.get_description(args);
+
+	// add possible argument inputs
+	auto modcod_format   = cli::Text(cli::Including_set("QPSK-S_8/9", "QPSK-S_3/5", "8PSK-S_3/5", "8PSK-S_8/9", "16APSK-S_8/9"));
+	auto src_type_format = cli::Text(cli::Including_set("RAND", "USER", "USER_BIN", "AZCW"                                    ));
+	args.add({"mod-cod"},            modcod_format,                                         "Modulation and coding scheme."       );
+	args.add({"chn-max-freq-shift"}, cli::Real(),                                           "Maximum Doppler shift."              );
+	args.add({"chn-max-delay"},      cli::Real(),                                           "Maximum Channel Delay."              );
+	args.add({"max-fe","e"},         cli::Integer(cli::Positive(), cli::Non_zero()),        "Modulation and coding scheme."       );
+	args.add({"sim-noise-min","m"},  cli::Real(),                                           "Min Eb/N0"                           );
+	args.add({"sim-noise-max","M"},  cli::Real(),                                           "Max Eb/N0"                           );
+	args.add({"sim-noise-step","s"}, cli::Real(),                                           "Step Eb/N0"                          );
+	args.add({"no-pll"},             cli::None(),                                           "Disable coarse PLL."                 );
+	args.add({"sim-debug", "d"},     cli::None(),                                           "Display debug."                      );
+	args.add({"sim-stats"},          cli::None(),                                           "Display stats."                      );
+	args.add({"src-type"},           src_type_format,                                       "Type of the binary source"           );
+	args.add({"src-path"},           cli::Text(),                                           "Path of the binary source"           );
+	args.add({"snk-path"},           cli::Text(),                                           "Path of the binary sink"             );
+	args.add({"dec-ite"},            cli::Integer(cli::Positive(), cli::Non_zero()),        "LDPC number of iterations"           );
+	args.add({"dec-implem"},         cli::Text(cli::Including_set("SPA", "MS", "NMS")),     "LDPC Implem "                        );
+	args.add({"dec-simd"},           cli::Text(cli::Including_set("INTER", "INTRA")),       "Display stats."                      );
+	args.add({"section"},            cli::Text(),                                           "Section to be used in bridge binary.");
+
+	// parse user arguments
+	arg_vals = ah.parse_arguments(args, cmd_warn, cmd_error);
+
+	// exit on wrong args
+	if (cmd_error.size())
+	{
+		if (cmd_error.size()) std::cerr << std::endl;
+		for (auto w = 0; w < (int)cmd_error.size(); w++)
+			std::cerr << rang::tag::error << cmd_error[w] << std::endl;
+
+		if (cmd_warn.size()) std::cerr << std::endl;
+		for (auto w = 0; w < (int)cmd_warn.size(); w++)
+			std::cerr << rang::tag::warning << cmd_warn[w] << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Params_DVBS2O::
+modcod_init(std::string modcod)
+{
 	if (modcod == "QPSK-S_8/9" || modcod == "")
 	{
 		mod = "QPSK";
@@ -89,62 +148,9 @@ Params_DVBS2O(int argc, char** argv)
 		src_path          = "../conf/src/K_14232.src";
 	}
 
-	src_path       = arg_vals.exist({"src-path"}          ) ? arg_vals.at      ({"src-path"}          ) : src_path    ;
-
 	itl_n_cols        = bps;
 	N_xfec_frame      = N_ldpc / bps;
 	N_pilots          = N_xfec_frame / (16 * M);
 	S                 = N_xfec_frame / M;
 	pl_frame_size     = M * (S + 1) + (N_pilots * P);
-
-	p_rad.N = (this->pl_frame_size) * 4; // 2 * N_fil
-	p_rad.store(arg_vals);
-}
-
-void Params_DVBS2O::
-get_arguments(int argc, char** argv, cli::Argument_map_value& arg_vals)
-{
-			// build argument handler
-		cli::Argument_handler ah(argc, (const char**) argv);
-		cli::Argument_map_info args;
-		std::vector<std::string> cmd_warn, cmd_error;
-
-		p_rad.get_description(args);
-
-		// add possible argument inputs
-		auto modcod_format   = cli::Text(cli::Including_set("QPSK-S_8/9", "QPSK-S_3/5", "8PSK-S_3/5", "8PSK-S_8/9", "16APSK-S_8/9"));
-		auto src_type_format = cli::Text(cli::Including_set("RAND", "USER", "USER_BIN", "AZCW"                                    ));
-		args.add({"mod-cod"},            modcod_format,                                         "Modulation and coding scheme."       );
-		args.add({"chn-max-freq-shift"}, cli::Real(),                                           "Maximum Doppler shift."              );
-		args.add({"chn-max-delay"},      cli::Real(),                                           "Maximum Channel Delay."              );
-		args.add({"max-fe","e"},         cli::Integer(cli::Positive(), cli::Non_zero()),        "Modulation and coding scheme."       );
-		args.add({"sim-noise-min","m"},  cli::Real(),                                           "Min Eb/N0"                           );
-		args.add({"sim-noise-max","M"},  cli::Real(),                                           "Max Eb/N0"                           );
-		args.add({"sim-noise-step","s"}, cli::Real(),                                           "Step Eb/N0"                          );
-		args.add({"no-pll"},             cli::None(),                                           "Disable coarse PLL."                 );
-		args.add({"sim-debug", "d"},     cli::None(),                                           "Display debug."                      );
-		args.add({"sim-stats"},          cli::None(),                                           "Display stats."                      );
-		args.add({"src-type"},           src_type_format,                                       "Type of the binary source"           );
-		args.add({"src-path"},           cli::Text(),                                           "Path of the binary source"           );
-		args.add({"snk-path"},           cli::Text(),                                           "Path of the binary sink"             );
-		args.add({"dec-ite"},            cli::Integer(cli::Positive(), cli::Non_zero()),        "LDPC number of iterations"           );
-		args.add({"dec-implem"},         cli::Text(cli::Including_set("SPA", "MS", "NMS")),     "LDPC Implem "                        );
-		args.add({"dec-simd"},           cli::Text(cli::Including_set("INTER", "INTRA")),       "Display stats."                      );
-		args.add({"section"},            cli::Text(),                                           "Section to be used in bridge binary.");
-
-		// parse user arguments
-		arg_vals = ah.parse_arguments(args, cmd_warn, cmd_error);
-
-		// exit on wrong args
-		if (cmd_error.size())
-		{
-			if (cmd_error.size()) std::cerr << std::endl;
-			for (auto w = 0; w < (int)cmd_error.size(); w++)
-				std::cerr << rang::tag::error << cmd_error[w] << std::endl;
-
-			if (cmd_warn.size()) std::cerr << std::endl;
-			for (auto w = 0; w < (int)cmd_warn.size(); w++)
-				std::cerr << rang::tag::warning << cmd_warn[w] << std::endl;
-			exit(EXIT_FAILURE);
-		}
 }
--- src/common/Params_DVBS2O/Params_DVBS2O.hpp
+++ src/common/Params_DVBS2O/Params_DVBS2O.hpp
@@ -9,26 +9,43 @@ using namespace factory;
 struct Params_DVBS2O
 {
 public:
-	factory::Radio p_rad;
+	// const
+	const int   N_ldpc    = 16200;
+	const int   M         = 90;    // number of symbols per slot
+	const int   P         = 36;    // number of symbols per pilot
+	const float rolloff   = 0.2;//DVBS2 0.05; // DVBS2-X
+	const int   osf       = 4;
+	const int   grp_delay = 50;
 
+	const std::vector<float> pilot_values = std::vector<float  > (P*2, std::sqrt(2.0f)/2.0f);
+	const std::vector<int  > pilot_start  = {1530, 3006, 4482, 5958, 7434};
+	const std::vector<int  > bch_prim_poly = {1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+
+	const std::string mat2aff_file_name = "../build/matlab_to_aff3ct.txt";
+	const std::string aff2mat_file_name = "../build/aff3ct_to_matlab.txt";
+
+	// no const
 	float ebn0_min;
 	float ebn0_max;
 	float ebn0_step;
-	bool debug;
-	bool stats;
-	bool no_pll;
-	int max_fe;       // max number of frame errors per SNR point
-	int max_n_frames; // max number of simulated frames per SNR point
-	int K_bch;
-	int N_bch;
-	int N_bch_unshortened;
-	int ldpc_nite;
-	int bps;
-	int N_xfec_frame;              // number of complex symbols
-	int N_pilots;
-	int S;                         // number of slots
-	int pl_frame_size;
-	int itl_n_cols;
+	float max_freq_shift;
+	float max_delay;
+	bool  debug;
+	bool  stats;
+	bool  no_pll;
+	int   max_fe;       // max number of frame errors per SNR point
+	int   max_n_frames; // max number of simulated frames per SNR point
+	int   K_bch;
+	int   N_bch;
+	int   N_bch_unshortened;
+	int   ldpc_nite;
+	int   bps;
+	int   N_xfec_frame;              // number of complex symbols
+	int   N_pilots;
+	int   S;                         // number of slots
+	int   pl_frame_size;
+	int   itl_n_cols;
+
 	std::string ldpc_implem;
 	std::string ldpc_simd;
 	std::string read_order;
@@ -41,24 +58,12 @@ public:
 	std::string src_path;
 	std::string sink_path;
 
-	const int   N_ldpc    = 16200;
-	const int   M         = 90;    // number of symbols per slot
-	const int   P         = 36;    // number of symbols per pilot
-	const float rolloff   = 0.2;//DVBS2 0.05; // DVBS2-X
-	const int   osf       = 4;
-	const int   grp_delay = 50;
+	factory::Radio p_rad;
 
-	float max_freq_shift;
-	float max_delay;
+private:
+	void modcod_init(std::string modcod);
 
-	const std::vector<float  > pilot_values = std::vector<float  > (P*2, std::sqrt(2.0f)/2.0f);
-	const std::vector<int    > pilot_start  = {1530, 3006, 4482, 5958, 7434};
-
-	const std::string mat2aff_file_name = "../build/matlab_to_aff3ct.txt";
-	const std::string aff2mat_file_name = "../build/aff3ct_to_matlab.txt";
-
-	const std::vector<int> bch_prim_poly = {1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-
+public:
 	Params_DVBS2O(int argc, char** argv);
 	void get_arguments(int argc, char** argv, cli::Argument_map_value& arg_vals);
 };
